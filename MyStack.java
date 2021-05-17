974590a494fcefcb76a3a83dc83b6d7eb7716504
Gilberto E. Cancel <45472419+GilbertCancel@users.noreply.github.com>
2021-05-17 08:47:31 -0400

    Add files via upload

---
+++ MyStack.java
@@ -0,0 +1,68 @@
+
+
+
+public class MyStack
+{
+
+  //Stack will be implemented through an array.
+  Card stack[];
+  public int top;
+  public int length;
+
+  //Initialize MyStack
+  public MyStack(){
+    this.stack = new Card[52];
+    top = -1;
+    length = 0;
+
+  }
+
+  //Push funtion implementation
+  public void push(Card ar[],Card val, int pos){
+
+    if(top + 1 ==  52){
+      System.out.print("Stack is Full");
+    }
+    else{
+      ar[pos] = val;
+    }
+
+  }
+
+  //top implementation
+  public Card top(){
+
+    if(top == -1){
+      return null;
+    }
+    else{
+      return stack[top];
+    }
+  }
+
+  //Pop implementation
+  public void pop(){
+
+    if(top == -1){
+      System.out.println("Stack is empty");
+    }
+    else{
+      top--;
+    }
+
+  }
+
+  //Checks if the Stack is empty
+  public Card isEmpty(){
+
+    if(top == -1){
+    return null;
+    }
+    else{
+      return(stack[top]);
+    }
+
+  }
+
+
+}
---
+++ Proj2.java
@@ -0,0 +1,9 @@
+
+public class Main{
+public static void main(String args[]){
+    {
+      Table t = new Table();
+      t.setVisible(true);
+    }
+  }
+}
